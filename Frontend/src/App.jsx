2921fff4a1bc0260bb0a0c64aafef07aefa550b3
Luis <luishuerta0518@gmail.com>
2025-02-20 15:19:27 -0800

    setup basic page routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,16 @@
-import { Hero, Footer, AllCategories, Subscribe, HowItWorks } from "./sections";
-import Nav from "./components/Nav";
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage";
+import JobListingsPage from "./pages/JobListingsPage";
+import CreateJobPage from "./pages/CreateJobPage";
+import ProfilePage from "./pages/ProfilePage";
+
 const App = () => (
-  <main className="relative">
-    <Nav/>
-    <section className="xl:padding-l wide:padding-r padding-b">
-      <Hero/>
-    </section>
-    <section className="padding">
-      <HowItWorks/>
-    </section> 
-    <section className="padding">
-      <AllCategories/>
-    </section>
-    <section className="padding-x sm:py-32 py-16 w-full">
-      <Subscribe/>
-    </section>
-    <section className="bg-black padding-x padding-t pb-8">
-      <Footer/>
-    </section>
-  </main>
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/listings" element={<JobListingsPage />} />
+    <Route path="/create" element={<CreateJobPage />} />
+    <Route path="/profile" element={<ProfilePage />} />
+  </Routes>
 );
 
 export default App;
---
+++ Frontend/src/pages/JobListingsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const JobListingsPage = () => {
+  return (
+    <div>JobListingsPage</div>
+  )
+}
+
+export default JobListingsPage
